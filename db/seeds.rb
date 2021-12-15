db5d4a98a23f38098bace61ee5bd107156af24b3
Zack <zackwilldev@gmail.com>
2021-12-15 14:54:55 -0500

    put wordlist.txt in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+word_list_file = File.open("./wordlist.txt")
+word_list_array = word_list_file.readlines.map(&:chomp)
+puts word_list_array[0..4]
